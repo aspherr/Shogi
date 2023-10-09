c6230a090d410c0d15d9517e41d56441a5974952
aspherr <Huzaifa.Syedd@gmail.com>
2023-10-09 20:35:50 +0100

    created pawn moveset

--- src/piece.py
+++ src/piece.py
@@ -92,6 +92,23 @@ class Pawn(Piece):
 
     def __repr__(self) -> str:
         return 'Pawn'
+    
+
+    def moves(self, board):
+
+        moves = []
+        captures = []
+        
+        self.boundaries(board)
+        if self.player == 'sente':
+            self.generate_moves(board, moves, captures, self.top, (self.rank-1, self.file))
+
+            return moves, captures
+        
+        elif self.player == 'gote':
+            self.generate_moves(board, moves, captures, self.bottom, (self.rank+1, self.file))
+
+            return moves, captures
 
 
 class Lance(Piece):
