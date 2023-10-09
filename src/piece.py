3a4ba7b926e2773b7955c9c6fed7a398d480f2fb
aspherr <Huzaifa.Syedd@gmail.com>
2023-10-09 21:02:23 +0100

    created gold general moveset

--- src/piece.py
+++ src/piece.py
@@ -237,6 +237,32 @@ class GoldGeneral(Piece):
 
     def __repr__(self) -> str:
         return 'Gold General'
+
+
+    def moves(self, board):
+
+        moves = []
+        captures = []
+        
+        self.boundaries(board)
+        if self.player == 'sente':
+            conditions = [self.top, self.right, self.left, self.bottom, self.top_right, self.top_left]
+            pos = [(self.rank-1, self.file), (self.rank, self.file+1), (self.rank, self.file-1),
+                   (self.rank+1, self.file), (self.rank-1, self.file+1), (self.rank-1, self.file-1)]
+            
+            for i in range(len(conditions)):
+                self.generate_moves(board, moves, captures, conditions[i], pos[i])
+
+        
+        elif self.player == 'gote':
+            conditions = [self.bottom, self.left, self.right, self.top, self.bottom_left, self.bottom_right]
+            pos = [(self.rank+1, self.file), (self.rank, self.file-1), (self.rank, self.file+1),
+                   (self.rank-1, self.file), (self.rank+1, self.file-1), (self.rank+1, self.file+1)]
+            
+            for i in range(len(conditions)):
+                self.generate_moves(board, moves, captures, conditions[i], pos[i])
+
+        return moves, captures
     
 
 class Bishop(Piece):
