90ad5515ec51a824ca49d97c20623a14268271e9
aspherr <Huzaifa.Syedd@gmail.com>
2023-10-09 20:58:59 +0100

    created silver general moveset

--- src/piece.py
+++ src/piece.py
@@ -203,6 +203,32 @@ class SilverGeneral(Piece):
         return 'Silver General'
 
 
+    def moves(self, board):
+
+        moves = []
+        captures = []
+        
+        self.boundaries(board)
+        if self.player == 'sente':
+            conditions = [self.top, self.top_right, self.top_left, self.bottom_right, self.bottom_left]
+            pos = [(self.rank-1, self.file), (self.rank-1, self.file+1), (self.rank-1, self.file-1), 
+                   (self.rank+1, self.file+1), (self.rank+1, self.file-1)]
+            
+            for i in range(len(conditions)):
+                self.generate_moves(board, moves, captures, conditions[i], pos[i])
+
+        
+        elif self.player == 'gote':
+            conditions = [self.bottom, self.bottom_left, self.bottom_right, self.top_left, self.top_right]
+            pos = [(self.rank+1, self.file), (self.rank+1, self.file-1), (self.rank+1, self.file+1),
+                   (self.rank-1, self.file-1), (self.rank-1, self.file+1)]
+            
+            for i in range(len(conditions)):
+                self.generate_moves(board, moves, captures, conditions[i], pos[i])
+
+        return moves, captures
+
+
 class GoldGeneral(Piece):
     id = 5
     def __init__(self, rank, file, player) -> None:
