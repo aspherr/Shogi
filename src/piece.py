c12793e0acfb047ecb68ab0a12212f38b345e6f9
aspherr <Huzaifa.Syedd@gmail.com>
2023-10-10 19:40:29 +0100

    created king moveset

--- src/piece.py
+++ src/piece.py
@@ -484,3 +484,33 @@ class King(Piece):
     def __repr__(self) -> str:
         return 'King'
 
+    def moves(self, board):
+
+        moves = []
+        captures = []
+        
+        self.boundaries(board)
+        if self.player == 'sente':
+            conditions = [self.top, self.right, self.left, self.bottom, 
+                          self.top_right, self.top_left, self.bottom_right, self.bottom_left]
+            
+            pos = [(self.rank-1, self.file), (self.rank, self.file+1), (self.rank, self.file-1), 
+                   (self.rank+1, self.file), (self.rank-1, self.file+1), (self.rank-1, self.file-1),
+                   (self.rank+1, self.file+1), (self.rank+1, self.file-1)]
+            
+            for i in range(len(conditions)):
+                self.generate_moves(board, moves, captures, conditions[i], pos[i])
+
+        
+        elif self.player == 'gote':
+            conditions = [self.top, self.right, self.left, self.bottom, 
+                          self.top_right, self.top_left, self.bottom_right, self.bottom_left]
+            
+            pos = [(self.rank-1, self.file), (self.rank, self.file+1), (self.rank, self.file-1), 
+                   (self.rank+1, self.file), (self.rank-1, self.file+1), (self.rank-1, self.file-1),
+                   (self.rank+1, self.file+1), (self.rank+1, self.file-1)]
+            
+            for i in range(len(conditions)):
+                self.generate_moves(board, moves, captures, conditions[i], pos[i])
+
+        return moves, captures
